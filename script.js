1e169a0cd61689c7ce7ff594d372612ff99a2d4a
ahmadarsafin09-glitch <ahmadarsafin09@gmail.com>
2026-07-18 18:16:59 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 function showMessage() {
-    document.getElementById("message").innerHTML =
-   Dengan penuh rasa syukur, <strong>Mas Budi</strong> dan
-<strong>Mba Suci</strong> mengucapkan terima kasih kepada seluruh keluarga,
-saudara, sahabat, dan para tamu yang telah hadir serta memberikan doa terbaik
-untuk putri tercinta kami. Kehadiran dan doa dari kalian merupakan hadiah
-yang sangat berarti bagi Khalisa. Semoga silaturahmi ini selalu terjaga dan
-Allah SWT melimpahkan keberkahan, kesehatan, serta kebahagiaan untuk kita
-semua.;
+  const message = `
+    Dengan penuh rasa syukur, <strong>Mas Budi</strong> dan
+    <strong>Mba Suci</strong> mengucapkan terima kasih kepada seluruh keluarga,
+    saudara, sahabat, dan para tamu yang telah hadir serta memberikan doa terbaik
+    untuk putri tercinta kami. Kehadiran dan doa dari kalian merupakan hadiah
+    yang sangat berarti bagi Khalisa. Semoga silaturahmi ini selalu terjaga dan
+    Allah SWT melimpahkan keberkahan, kesehatan, serta kebahagiaan untuk kita semua.
+
+    <br><br>
+
+    Semoga setiap langkah kecil Khalisa dipenuhi kebahagiaan, kasih sayang, dan keberkahan. Tumbuhlah menjadi anak yang sholehah, sehat, cerdas, serta menjadi kebanggaan Ayah Mas Budi, Bunda Mba Suci, dan seluruh keluarga. Selamat Ulang Tahun yang ke-2, Khalisa Syafa Azzahra. 🤍🎉
+  `;
+
+  document.getElementById("message").innerHTML = message;
 }
