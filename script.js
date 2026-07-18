6b82c8606261c2be8fc11c4b476b46e5084f662a
ahmadarsafin09-glitch <ahmadarsafin09@gmail.com>
2026-07-18 18:58:07 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function showMessage() {
     document.getElementById("message").innerHTML =
-    "🎂 "Semoga setiap langkah kecil Khalisa dipenuhi kebahagiaan, kasih sayang, dan keberkahan. Tumbuhlah menjadi anak yang sholehah, sehat, cerdas, serta menjadi kebanggaan Ayah Mas Budi, Bunda Mba Suci, dan seluruh keluarga. Selamat Ulang Tahun yang ke-2, Khalisa Syafa Azzahra. 🤍🎉" 🎉";
+    "🎂 Selamat Ulang Tahun! Semoga semua impianmu tercapai dan kebahagiaan selalu menyertaimu. 🎉";
 }
