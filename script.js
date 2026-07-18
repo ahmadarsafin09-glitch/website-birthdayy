f138843d4fbba69537344da6c2cf61bd002a58a6
ahmadarsafin09-glitch <ahmadarsafin09@gmail.com>
2026-07-18 18:06:15 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function showMessage() {
     document.getElementById("message").innerHTML =
-    "🎂 Selamat Ulang Tahun! Semoga semua impianmu tercapai dan kebahagiaan selalu menyertaimu. 🎉";
+   Dengan penuh rasa syukur, <strong>Mas Budi</strong> dan
+<strong>Mba Suci</strong> mengucapkan terima kasih kepada seluruh keluarga,
+saudara, sahabat, dan para tamu yang telah hadir serta memberikan doa terbaik
+untuk putri tercinta kami. Kehadiran dan doa dari kalian merupakan hadiah
+yang sangat berarti bagi Khalisa. Semoga silaturahmi ini selalu terjaga dan
+Allah SWT melimpahkan keberkahan, kesehatan, serta kebahagiaan untuk kita
+semua.;
 }
